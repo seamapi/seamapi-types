fdfbf4f3cd784071ed1dc5f26a8ea3a470a419da
Tristan Brewster <tristan@getseam.com>
2022-10-28 15:31:07 -0600

    feat: Add `Workspace` type

---
+++ src/models.ts
@@ -0,0 +1,8 @@
+export interface Workspace {
+  workspace_id: string
+  connect_partner_name?: string
+  name: string
+  is_sandbox: boolean
+  webview_inviter_name?: string
+  webview_inviter_logo_url?: string
+}
