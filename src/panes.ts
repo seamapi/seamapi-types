186db8526041091387ef0c855c4795dec771784f
Abimael Martell <abimex@gmail.com>
2023-04-03 16:12:12 -0700

    Move to credential type

--- src/panes.ts
+++ src/panes.ts
@@ -86,7 +86,7 @@ export type LoginPane = Pane<
   "login_pane",
   {
     accepted_user_identifiers: Array<"email" | "phone" | "username">
-    context?: "smartthings_pre_auth" | "igloo_developer_auth"
+    credential?: "password" | "api_key"
     default_user_identifier?: string
     provider: ProviderMetadata
   },
